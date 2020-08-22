c69a7f74005332a69c4d6416c4bc872b6829e1db
Carlo Paredes <c.nick.paredes@gmail.com>
2020-08-22 10:18:42 -0700

    Changed email regexp

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -127,7 +127,8 @@ class Signup extends React.Component {
 
     handleEmailError = (event) => {
         const email = event.target.value
-        const regexp_email = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        // const regexp_email = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        const regexp_email = /@/
 
         if (email.length === 0) {
             this.setState({
